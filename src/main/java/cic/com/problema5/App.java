3155f6b0b80dae56d736f78e6d03fd55a1e74981
Alex <alexcanocos@gmail.com>
2017-07-05 12:15:03 +0200

    first commit

---
+++ src/main/java/cic/com/problema5/App.java
@@ -0,0 +1,13 @@
+package cic.com.problema5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
